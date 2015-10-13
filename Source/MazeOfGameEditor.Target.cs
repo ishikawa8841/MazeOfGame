7b58f1915ec52cd55d25bfdeea2027451bf73f11
Ishikawa_Takaaki <ishikawa8841@gmail.com>
2015-10-13 11:23:57 +0900

    ue4コントロール用のCSファイルも追加する

---
+++ Source/MazeOfGameEditor.Target.cs
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class MazeOfGameEditorTarget : TargetRules
+{
+	public MazeOfGameEditorTarget(TargetInfo Target)
+	{
+		Type = TargetType.Editor;
+	}
+
+	//
+	// TargetRules interface.
+	//
+
+	public override void SetupBinaries(
+		TargetInfo Target,
+		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
+		ref List<string> OutExtraModuleNames
+		)
+	{
+		OutExtraModuleNames.AddRange( new string[] { "MazeOfGame" } );
+	}
+}
