d1b0bdba1feedbd22ecc2d15f63f67fac94b8ae3
Ishikawa_Takaaki <ishikawa8841@gmail.com>
2015-10-13 17:06:34 +0900

    自動ビルドテスト用バッチファイル

---
+++ AutoBuild/MazeOfGame_Build_Development.bat
@@ -0,0 +1,2 @@
+call "C:\Program Files\Epic Games\4.9\Engine\Build\BatchFiles\Build.bat" MazeOfGameEditor Win64 Development "C:\Users\usr10049220\Documents\Unreal Projects\MazeOfGame\MazeOfGame.uproject" -rocket
+pause
