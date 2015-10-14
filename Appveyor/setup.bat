2cb1f90ca3214a11df18fca5ae058390cdbafa53
Ishikawa_Takaaki <ishikawa8841@gmail.com>
2015-10-14 12:45:29 +0900

    更新

--- Appveyor/setup.bat
+++ Appveyor/setup.bat
@@ -1,5 +1,7 @@
 set GIT="C:\Program Files (x86)\Git\bin\git.exe"
 
-%GIT% clone --branch=4.9  https://github.com/EpicGames/UnrealEngine.git c:\GitHub\
+ls -l
+
+%GIT% clone --branch=4.9 https://github.com/EpicGames/UnrealEngine.git c:\GitHub\
 
 pause
